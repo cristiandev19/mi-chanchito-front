6c95713893e02a660cb016d017152c6533283df8
cristiandev <cristiansotomayor.dev@gmail.com>
2021-04-04 01:30:33 -0500

    emitiendo

--- src/components/TransferCard.jsx
+++ src/components/TransferCard.jsx
@@ -1,46 +1,67 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { cashFlowLabels } from '../constant/transfer';
+import { cashFlowLabels, eventsTransferCard } from '../constant/transfer';
 
 const TransferCard = ({
   title,
   amount,
   casflow,
+  transferCardEvent,
 }) => {
   let labelCard;
-
+  let amountCard;
   switch (casflow) {
     case cashFlowLabels.ingreso:
       labelCard = <div className={`transfer-card__label ${cashFlowLabels.ingreso}`}>Ingreso</div>;
+      amountCard = <div>{`+ S/. ${amount}`}</div>;
       break;
     case cashFlowLabels.egreso:
       labelCard = <div className={`transfer-card__label ${cashFlowLabels.egreso}`}>Egreso</div>;
+      amountCard = <div>{`- S/. ${amount}`}</div>;
       break;
 
     default:
       break;
   }
 
+  const emitUpdate = () => {
+    transferCardEvent({
+      type: eventsTransferCard.update,
+    });
+  };
+
+  const handleKeyDownUpdate = (e) => {
+    if (e.code === 'KeyU') {
+      console.log('e', e.code);
+    }
+  };
+
   return (
-    <div className="card transfer-card">
+    <div
+      className="card transfer-card"
+      onClick={emitUpdate}
+      onKeyDown={handleKeyDownUpdate}
+      role="button"
+      tabIndex="0"
+    >
       <div>
         {title}
       </div>
       <span className="spacer" />
-      <div>
-        { `S/. ${amount}`}
-      </div>
+      {
+        amountCard
+      }
       {
         labelCard
       }
     </div>
   );
 };
-
 TransferCard.propTypes = {
-  title   : PropTypes.string.isRequired,
-  amount  : PropTypes.number.isRequired,
-  casflow : PropTypes.string.isRequired,
+  title             : PropTypes.string.isRequired,
+  amount            : PropTypes.number.isRequired,
+  casflow           : PropTypes.string.isRequired,
+  transferCardEvent : PropTypes.func.isRequired,
 };
 
 export default TransferCard;
