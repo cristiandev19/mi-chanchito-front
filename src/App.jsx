09f2a3f12d873fadf7803dcfd93338e92bb11228
cristiandev <cristiansotomayor.dev@gmail.com>
2021-04-20 21:10:04 -0500

    cambiando el app

--- src/App.jsx
+++ src/App.jsx
@@ -1,128 +1,34 @@
 import './App.css';
-import Button from '@material-ui/core/Button';
-import { useState } from 'react';
-import { makeStyles } from '@material-ui/core';
-import BalanceCard from './components/BalanceCard';
-import TransferCard from './components/TransferCard';
-import { actionsTransfer, eventsTransferDialog, eventsTransferCard } from './constant/transfer';
-import TransferDialog from './components/TransferDialog';
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    '& > *': {
-      margin: theme.spacing(1),
-    },
-  },
-  addBtn: {
-    width  : '100%',
-    margin : '20px 0',
-  },
-}));
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+import AuthPage from './pages/AuthPage';
+import HomePage from './pages/HomePage';
 
 function App() {
-  const classes = useStyles();
-  const [openTransferDialog, setOpenTransferDialog] = useState(false);
-  const [actionTransfer, setActionTransfer] = useState(actionsTransfer.create);
-  const data = [
-    {
-      title       : 'ingreso 1',
-      description : 'descripcion ingreso 1',
-      amount      : 450,
-      date        : Date.now(),
-      casflow     : 'ingreso',
-    },
-    {
-      title       : 'egreso 1',
-      description : 'descripcion egreso 1',
-      amount      : 200,
-      date        : Date.now(),
-      casflow     : 'egreso',
-    },
-    {
-      title       : 'ingreso 2',
-      description : 'descripcion ingreso 1',
-      amount      : 500,
-      date        : Date.now(),
-      casflow     : 'ingreso',
-    },
-    {
-      title       : 'egreso 2',
-      description : 'descripcion egreso 1',
-      amount      : 500,
-      date        : Date.now(),
-      casflow     : 'egreso',
-    },
-  ];
-  console.log('data', data);
-
-  const handleAddTransfer = () => {
-    console.log('hey');
-    setActionTransfer(actionsTransfer.create);
-    setOpenTransferDialog(true);
-  };
-
-  const handleTransferDialogEvent = ({ type, payload }) => {
-    console.log('payload', payload);
-    switch (type) {
-      case eventsTransferDialog.close:
-        setOpenTransferDialog(false);
-        break;
-      case eventsTransferDialog.create:
-        console.log('creando');
-        break;
-      case eventsTransferDialog.update:
-        console.log('actualizando');
-        break;
-      default:
-        break;
-    }
-  };
-
-  const handleTransferCardEvent = ({ type, payload }) => {
-    console.log({ payload });
-    switch (type) {
-      case eventsTransferCard.update:
-        console.log('actualizando');
-        break;
-      default:
-        break;
-    }
-  };
-
+  console.log('app');
   return (
-    <div className="App">
-      <div className="container">
-        <h1>hola</h1>
-
-        <BalanceCard data={data} />
-
-        <Button
-          variant="contained"
-          color="primary"
-          className={classes.addBtn}
-          onClick={handleAddTransfer}
-        >
-          Agregar
-        </Button>
-        {/* cards */}
-        {
-          data.map((transfer) => (
-            <TransferCard
-              title={transfer.title}
-              amount={transfer.amount}
-              casflow={transfer.casflow}
-              key={transfer.title}
-              transferCardEvent={handleTransferCardEvent}
-            />
-          ))
-        }
+    // <div className='App">
+    //   <h1>
+    //     prieba
+    //   </h1>
+    // </div>
+    <Router>
+      <div>
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/auth">
+            <AuthPage />
+          </Route>
+          <Route path="/">
+            <HomePage />
+          </Route>
+        </Switch>
       </div>
-      <TransferDialog
-        open={openTransferDialog}
-        actionTransfer={actionTransfer}
-        transferDialogEvent={handleTransferDialogEvent}
-      />
-    </div>
+    </Router>
   );
 }
 
