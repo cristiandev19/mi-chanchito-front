f334b0ce48c34bfeb12d4e1bb8ae1fa764a9dbfe
cristiandev <cristiansotomayor.dev@gmail.com>
2021-04-20 21:10:26 -0500

    home page

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+  console.log('hey you');
+  return (
+    <div>
+      <h1>Hola estamos en el home</h1>
+    </div>
+  );
+};
+
+export default HomePage;
