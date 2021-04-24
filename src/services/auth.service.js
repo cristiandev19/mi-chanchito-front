b40ef36bb33313ba51276c283d4c8d2729d8283d
cristiandev <cristiansotomayor.dev@gmail.com>
2021-04-24 00:26:09 -0500

    un nuevo servicio porque extrano angular

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,23 +1,67 @@
 const BACK_URL = 'http://localhost:8089';
 
-exports.loginEmail = ({
-  email, password,
-}) => new Promise(async (resolve) => {
-  try {
-    const bodyObject = {
-      email, password,
-    };
-    const response = await fetch(`${BACK_URL}/auth/login-email`, {
-      method  : 'POST',
-      headers : {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(bodyObject),
-    });
-    const data = await response.json();
-    console.log('data ', data);
-    return resolve({ success: true, payload: [] });
-  } catch (error) {
-    return resolve({ error });
+class AuthService {
+  constructor() {
+    this.path = 'auth';
   }
-});
+
+  loginEmail({
+    email, password,
+  }) {
+    return new Promise(async (resolve) => {
+      try {
+        const bodyObject = {
+          email, password,
+        };
+        const response = await fetch(`${BACK_URL}/${this.path}/login-email`, {
+          method  : 'POST',
+          headers : {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(bodyObject),
+        });
+        const data = await response.json();
+        console.log('data ', data);
+        return resolve({ success: true, payload: data.user });
+      } catch (error) {
+        return resolve({ error });
+      }
+    });
+  }
+
+  signupEmail({
+    email, password,
+  }) {
+    return new Promise(async (resolve) => {
+      try {
+        const bodyObject = {
+          email, password,
+        };
+        const response = await fetch(`${BACK_URL}/${this.path}/signup-email`, {
+          method  : 'POST',
+          headers : {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(bodyObject),
+        });
+        const data = await response.json();
+        console.log('data ', data);
+        return resolve({ success: true, payload: [] });
+      } catch (error) {
+        return resolve({ error });
+      }
+    });
+  }
+
+  setLocalStorage({
+    email, profile, token, expiresIn,
+  }) {
+    const userObj = {
+      email, profile, token, expiresIn,
+    };
+    console.log('userObj', userObj);
+    localStorage.setItem('user', JSON.stringify({ ...userObj }));
+  }
+}
+
+export default AuthService;
