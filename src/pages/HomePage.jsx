a5b210602542f2cb85d23a30aa36e74d83454024
cristiandev <cristiansotomayor.dev@gmail.com>
2021-04-25 22:24:08 -0500

    home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const HomePage = () => {
   console.log('hey you');
   return (
     <div>
       <h1>Hola estamos en el home</h1>
+      <h2>Bienvenido a mi chanchito</h2>
+      <Link to="/auth">Auth</Link>
     </div>
   );
 };
