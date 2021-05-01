833c6a0c5abd2b6d796c11d64dcba451258f6a5d
cristiandev <cristiansotomayor.dev@gmail.com>
2021-04-30 23:24:13 -0500

    servicio update y delete

--- src/services/transfer.service.js
+++ src/services/transfer.service.js
@@ -65,6 +65,75 @@ class TransferService {
       }
     });
   }
+
+  updateTransfer({
+    details,
+    description,
+    amount,
+    dateTransfer,
+    cashFlow,
+    idTransfer,
+  }) {
+    return new Promise(async (resolve) => {
+      try {
+        const user = JSON.parse(localStorage.getItem('user'));
+        if (!user) throw new Error('No hay un usuario');
+        const { token } = user;
+
+        console.log('idTransfer', idTransfer);
+        const postObj = {
+          details,
+          description,
+          amount,
+          dateTransfer,
+          cashFlow,
+          _id: idTransfer,
+        };
+
+        const response = await fetch(`${config.backendUrl}/${this.path}/update`, {
+          method  : 'POST',
+          headers : {
+            'Content-Type' : 'application/json',
+            Authorization  : token,
+          },
+          body: JSON.stringify(postObj),
+        });
+        const data = await response.json();
+        console.log('data ', data);
+        return resolve({ success: true, payload: data });
+      } catch (error) {
+        return resolve({ error });
+      }
+    });
+  }
+
+  deleteTransfer({
+    idTransfer,
+  }) {
+    return new Promise(async (resolve) => {
+      try {
+        const user = JSON.parse(localStorage.getItem('user'));
+        if (!user) throw new Error('No hay un usuario');
+        const { token } = user;
+
+        const postObj = { idTransfer };
+
+        const response = await fetch(`${config.backendUrl}/${this.path}/delete`, {
+          method  : 'POST',
+          headers : {
+            'Content-Type' : 'application/json',
+            Authorization  : token,
+          },
+          body: JSON.stringify(postObj),
+        });
+        const data = await response.json();
+        console.log('data ', data);
+        return resolve({ success: true, payload: data });
+      } catch (error) {
+        return resolve({ error });
+      }
+    });
+  }
 }
 
 export default TransferService;
