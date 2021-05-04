6b64674d46a4bacf66fcae8f8201f4d102dc12d5
cristiandev <cristiansotomayor.dev@gmail.com>
2021-05-03 19:22:34 -0500

    cosillas del profile

---
+++ src/pages/ProfilePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ProfilePage = () => {
+  console.log('hey');
+  return (
+    <>
+      <h1>este es el perfil</h1>
+    </>
+  );
+};
+
+export default ProfilePage;
