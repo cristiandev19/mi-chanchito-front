84712ea4fb414d112fc85a48f8cf6c2f664db8e6
cristiandev <cristiansotomayor.dev@gmail.com>
2021-05-03 23:24:50 -0500

    nueva pagina

---
+++ src/pages/DashboardGeneralBalancePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const DashboardGeneralBalancePage = () => {
+  console.log('hol');
+  return (
+    <div>
+      <h1>
+        En esta pagina ira en balance general
+      </h1>
+    </div>
+  );
+};
+
+export default DashboardGeneralBalancePage;
