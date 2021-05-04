5e8ab19e7c23980ebe7bffbfec37689790a3990f
cristiandev <cristiansotomayor.dev@gmail.com>
2021-05-03 23:24:44 -0500

    format date para ver en card

--- src/lib/date.js
+++ src/lib/date.js
@@ -7,3 +7,11 @@ exports.formatInputDate = (date) => {
   const day = addLeadingZeros(currentDatetime.getDate());
   return `${year}-${month}-${day}`;
 };
+
+exports.formatDate = (date, sign = '-') => {
+  const currentDatetime = new Date(date);
+  const year = currentDatetime.getFullYear();
+  const month = addLeadingZeros(currentDatetime.getMonth() + 1);
+  const day = addLeadingZeros(currentDatetime.getDate());
+  return `${day}${sign}${month}${sign}${year}`;
+};
